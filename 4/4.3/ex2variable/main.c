00d0e6c231ecb9b7643bfbe5d2b9c9886f3a7d18
Ref-ra1n <zhangyt200261@outlook.com>
2025-11-12 16:05:42 +0800

    add

---
+++ 4/4.3/ex2variable/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  /*  int i;
+    printf("sizeof(i) =  %d\n",sizeof (i));
+    double d1,d2;
+    printf("sizeof(d1) =  %d,sizeof(d2) =  %d,sizeof(double) =  %d\n",
+           sizeof(d1), sizeof(d2), sizeof(double));
+    //数据类型决定内存大小*/
+  int i = 3;
+  i = 4;
+  i = i + 1;
+    return 0;
+}
