196b1d8dc213c9f3014da46551e94884adc1006f
Ref-ra1n <zhangyt200261@outlook.com>
2025-11-13 15:47:36 +0800

    add

---
+++ 4/4.3/ex6default/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   /* float f = 1/2;
+    printf("f = %f\n",f);//f = 0.000000*/
+ /*   float f = 1.0/2;
+    printf("f = %f\n",f);//f = 0.500000*/
+    long long ll;
+   /* ll = 131072*131072;
+    printf("ll = %lld",ll);//ll = 0*/
+    ll = (long long )131072*131072;
+    printf("ll = %lld",ll);//ll = 17179869184
+    return 0;
+}
