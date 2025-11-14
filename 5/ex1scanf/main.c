5fa33589f8575e0b310c8d47915e4dde8d62a2bc
Ref-ra1n <zhangyt200261@outlook.com>
2025-11-14 17:05:14 +0800

    add

---
+++ 5/ex1scanf/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i;//用来存放从stdin过来的数据
+    int ret = scanf("%d",&i);
+    //%d是一个控制符，用来说将要接收的数据是整数
+    //&i 将数据存入i的地址当中，&符号取地址，不可省略
+    printf("ret = %d,i = %d\n",ret,i);
+    return 0;
+}
