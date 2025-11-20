421366e48369aa60d59451c4d24ba30ff6e4902d
Ref-ra1n <zhangyt200261@outlook.com>
2025-11-20 15:46:24 +0800

    add

---
+++ 6/ex4assignment/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    /*int i = 1;
+    i ++;//等价于i+=1 等价于i = i+1
+    printf("i = %d\n",i);*/
+    int x,y;
+    x = y = 1;
+    printf("x= %d,y = %d",x,y);
+    return 0;
+}
