f2168116feb0304d62c363ca1fe3d672d77d418d
Ref-ra1n <zhangyt200261@outlook.com>
2025-11-21 10:32:26 +0800

    add

---
+++ 7/ex1singleif/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    scanf("%d",&i);
+    if(i == 1){
+        printf("i is 1!\n");
+        printf("i is 1!\n");
+    }//如果不写花括号，if只包含第一个printf
+    return 0;
+}
