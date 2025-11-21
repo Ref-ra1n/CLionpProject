04191b20a5a94a010e4f92c27ee1a8ac80f8ca95
Ref-ra1n <zhangyt200261@outlook.com>
2025-11-21 16:18:40 +0800

    add

---
+++ 7/ex4proximity/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+        if (i>1);
+         if(i<10)
+         printf("i is between 1 and 10\n");
+        else printf("i is less than 1\n");
+        //输出i is between 1 and 10
+    return 0;
+}
