8cdcf8405cfcac0e5676bb6ccc8b778a9128e422
Ref-ra1n <zhangyt200261@outlook.com>
2025-12-17 20:03:03 +0800

    add

--- 12/ex4pointer-array/main.c
+++ 12/ex4pointer-array/main.c
@@ -7,7 +7,11 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         printf("i = %d,p+i = %p,&arr[i] = %p\n",i,p+i,&arr[i]);
         printf("i = %d,*(p+i) = %d,arr[i] = %d\n",i,*(p+i),arr[i]);
-        printf("i = %d,p[i] = %d,*(arr+i) = %d\n",i,p[i],*(arr+i));
+        printf("i = %d,i[p] = %d,*(arr+i) = %d\n",i,i[p],*(arr+i));
     }
+    printf("sizeof(p) = %d,sizeof(arr) = %d",
+           sizeof(p), sizeof(arr));
+    //sizeof(p) = 8,sizeof(arr) = 20
+    //p是一个地址64个比特，8个字节；arr是一个有5个int的数组，4*5 = 20
     return 0;
 }
