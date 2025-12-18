ca034f94746a9b0091bca759228728782aff9d63
Ref-ra1n <zhangyt200261@outlook.com>
2025-12-18 11:50:36 +0800

    add

--- 12/ex5rules/main.c
+++ 12/ex5rules/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-int func(){
+#include <stdlib.h>
+/*int* func(){
     int arr[] = {1,2,3};
+    for (int i = 0; i < 3; ++i) {
+        printf("func arr[i] = %d\n",arr[i]);
+    }
+    return arr;
+}*/
+int* func(){
+    int* arr = (int*)malloc(3* sizeof(int ));
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
     for (int i = 0; i < 3; ++i) {
         printf("func arr[i] = %d\n",arr[i]);
     }
@@ -11,5 +22,6 @@ int main() {
     for (int i = 0; i < 3; ++i) {
         printf("main p[i] = %d\n",p[i]);
     }
+    free (p);//避免内存泄露
     return 0;
 }
