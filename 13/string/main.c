3c000a652cbbab39b33f45c1ee9dd9a35fb40c58
Ref-ra1n <zhangyt200261@outlook.com>
2026-01-13 15:49:21 +0800

    add

---
+++ 13/string/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    /*char str[] = "hello";
+    printf("str = %s\n",str);*/
+    char str[6] = {'h','e','l','l','o','\0'};
+    printf("str = %s\n",str);
+    return 0;
+}
