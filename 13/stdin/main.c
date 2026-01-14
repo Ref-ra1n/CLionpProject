d77660d0a5e10aaae5aaa56f78f42fdd87fa6209
Ref-ra1n <zhangyt200261@outlook.com>
2026-01-14 11:03:00 +0800

    add

--- 13/stdin/main.c
+++ 13/stdin/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    char str[20];
-    /*scanf("%s",str);//str是一个数组，作为参数传递会退化为指针
-    printf("str = %s\n",str);
-    //how are you  --->  str = how*/
+    char str[10];
+/*    scanf("%s",str);//str是一个数组，作为参数传递会退化为指针
+    printf("str = %s\n",str);*/
+    //how are you  --->  str = how
 
-    fgets(str,20,stdin);
+/*    fgets(str,20,stdin);
+    printf("str = %s\n",str);//会保留换行符，有两行空行*/
+    fgets(str,10,stdin);
     int idx = strlen(str) -1;
     if(str[idx] == '\n'){
         str[idx] = '\0';
-    }
-    printf("str = %s\n",str);//会保留换行符，有两行空行
+    }//将最后的换行符替换为空
+    printf("str = %s\n",str);
+    //how are you -->str = how are y
     return 0;
 }
