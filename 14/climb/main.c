47c25cab830c7281b12d08029c0f3a12ffb86c68
Ref-ra1n <zhangyt200261@outlook.com>
2026-01-14 17:04:25 +0800

    add

--- 14/climb/main.c
+++ 14/climb/main.c
@@ -3,7 +3,22 @@
 假设你正在爬一个楼梯，它总共有n阶台阶
 每次你只能爬1或2阶
 问：你有多少种不同的方法可以爬到楼顶*/
+int f(int n){
+    //大问题--->小问题
+    if(n>2){
+        int result = f(n-1) + f(n-2);
+        return result;
+    }
+    //最小问题的解决方案
+    if(n == 2){
+        return 2;
+    }
+    if(n == 1){
+        return 1;
+    }
+}
 int main() {
-    printf("Hello, World!\n");
+    int n = 4;
+    printf("f(n) = %d\n",f(4));
     return 0;
 }
