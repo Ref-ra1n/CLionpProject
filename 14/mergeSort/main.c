453dcb3afb0d84a1a60f68708e9939142838a46b
Ref-ra1n <zhangyt200261@outlook.com>
2026-02-02 17:12:55 +0800

    add

---
+++ 14/mergeSort/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void merge(int* arr,int* temp,int left,int mid,int right){
+    //合并arr[left]~arr[mid]与arr[mid+1]~arr[right]
+    //1.将arr的内容备份到temp里面
+
+    //i访问左半边，j访问右半边，k存放结果
+}
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
